12042978f4fc14eda7bae5a0d6e32dc2aad752b2
Nicolas Lepage <19571875+nlepage@users.noreply.github.com>
2019-07-05 00:03:05 +0200

    :bulb:

--- graphql/index.js
+++ graphql/index.js
@@ -1,3 +1,5 @@
 import { call } from '@cuillere/core'
 
 export const makeResolverFactory = run => genFn => (obj, args, ctx, info) => run(ctx)(call(genFn, obj, args, ctx, info))
+
+// FIXME a makeResolvers which takes a tree of generator functions and returns a tree of resolvers
