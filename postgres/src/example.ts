996ac80619f4998ca81e1977da49e16eb127e3ad
Nicolas Lepage <19571875+nlepage@users.noreply.github.com>
2019-07-06 00:04:29 +0200

    :sparkles: Support multiple pools

--- postgres/src/example.ts
+++ postgres/src/example.ts
@@ -2,24 +2,34 @@ import { makeRunner, call } from '@cuillere/core'
 import { poolMiddleware, queryMiddleware, query, end } from './'
 
 const run = makeRunner(
-    poolMiddleware({
-        database: 'postgres',
-        user: 'postgres',
-        password:'password',
-        port: 32768,
-    }),
+    poolMiddleware(
+        {
+            name: 'foo',
+            database: 'postgres',
+            user: 'postgres',
+            password: 'password',
+            port: 32768,
+        },
+        {
+            name: 'bar',
+            database: 'postgres',
+            user: 'postgres',
+            password: 'password',
+            port: 32769,
+        },
+    ),
     queryMiddleware(),
 );
 
 (async () => {
     await Promise.all([
-        run()(call(function*() {
-            console.log(yield query('SELECT NOW()'))
-            console.log(yield query('SELECT NOW()'))
+        run()(call(function* () {
+            console.log(yield query({ text: 'SELECT NOW()', pool: 'foo' }))
+            console.log(yield query({ text: 'SELECT NOW()', pool: 'bar' }))
         })),
-        run()(call(function*() {
-            console.log(yield query('SELECT NOW()'))
-            console.log(yield query('SELECT NOW()'))
+        run()(call(function* () {
+            console.log(yield query({ text: 'SELECT NOW()', pool: 'foo' }))
+            console.log(yield query({ text: 'SELECT NOW()', pool: 'bar' }))
         })),
     ])
 
--- postgres/src/index.ts
+++ postgres/src/index.ts
@@ -1,8 +1,14 @@
-import { Pool, PoolConfig, PoolClient } from 'pg'
+import { Pool, PoolConfig as PgPoolConfig, PoolClient, QueryConfig as PgQueryConfig } from 'pg'
 import { Middleware, isStart } from '@cuillere/core'
 
 const GET_CLIENT = Symbol('GET_CLIENT')
-const CLIENT = Symbol('CLIENT')
+const CLIENTS = Symbol('CLIENTS')
+const DEFAULT_POOL = 'DEFAULT POOL'
+
+const values = obj => [
+    ...Object.values(obj),
+    ...Object.getOwnPropertySymbols(obj).map(symbol => obj[symbol]),
+]
 
 const END = Symbol('END')
 
@@ -18,32 +24,57 @@ function isEnd(operation: any): operation is End {
     return operation && operation[END]
 }
 
-export function makePool(poolConfig?: PoolConfig) {
-    const pool = new Pool(poolConfig)
+interface PoolConfig extends PgPoolConfig {
+    name?: string;
+}
+
+export function makePool(...poolConfigs: PoolConfig[]) {
+    const pools: any = {}
+
+    if (poolConfigs.length <= 1) {
+        const [poolConfig] = poolConfigs
+        const name = (poolConfig && poolConfig.name) || DEFAULT_POOL
+        pools[name] = new Pool(poolConfig)
+    } else {
+        poolConfigs.forEach(({ name, ...poolConfig }) => {
+            if (!name) throw new TypeError('Each pool config should have a name')
+            pools[name] = new Pool(poolConfig)
+        })
+    }
 
     const execute = async (ctx: any, cb: Function) => {
-        ctx[GET_CLIENT] = async () => {
-            let client = ctx[CLIENT]
-            if (!client) ctx[CLIENT] = client = await pool.connect()
+        if (!ctx[CLIENTS]) ctx[CLIENTS] = {}
+
+        ctx[GET_CLIENT] = async (name?: string) => {
+            const clientName = name || DEFAULT_POOL
+            const pool = pools[clientName]
+            let client = ctx[CLIENTS][clientName]
+            if (!client) ctx[CLIENTS][clientName] = client = await pool.connect()
             return client
         }
 
         try {
             return await cb()
         } finally {
-            if (ctx[CLIENT]) await ctx[CLIENT].release()
+            await values(ctx[CLIENTS]).reduce(async (acc, client: any) => {
+                await acc
+                await client.release()
+            }, Promise.resolve())
         }
     }
 
-    execute.end = () => pool.end()
+    execute.end = (): any => values(pools).reduce(async (acc, pool: any) => {
+        await acc
+        await pool.end()
+    }, Promise.resolve())
 
     return execute
 }
 
-export const getClient = async (ctx: any): Promise<PoolClient> => ctx[GET_CLIENT]()
+export const getClient = async (ctx: any, name?: string): Promise<PoolClient> => ctx[GET_CLIENT](name)
 
-export function poolMiddleware(poolConfig?: PoolConfig): Middleware {
-    const pool = makePool(poolConfig)
+export function poolMiddleware(...poolConfigs: PoolConfig[]): Middleware {
+    const pool = makePool(...poolConfigs)
 
     return next => async (operation, ctx) => {
         if (isStart(operation)) {
@@ -58,31 +89,33 @@ export function poolMiddleware(poolConfig?: PoolConfig): Middleware {
     }
 }
 
+interface QueryConfig extends PgQueryConfig {
+    pool?: string;
+}
+
 const QUERY = Symbol('QUERY')
 
 interface Query {
-    [QUERY]: true,
-    sql: string,
+    [QUERY]: true;
+    config: QueryConfig;
 }
 
-export function query(sql: string): Query {
-    return {
-        [QUERY]: true,
-        sql,
-    }
-}
+// FIXME support same signatures as pg's query
+export const query = (config: QueryConfig): Query => ({
+    [QUERY]: true,
+    config,
+})
 
 function isQuery(operation: any): operation is Query {
     return operation && operation[QUERY]
 }
 
-export function queryMiddleware(): Middleware {
+export const queryMiddleware = (): Middleware => next => async (operation, ctx) => {
+    if (!isQuery(operation)) return next(operation)
 
-    return next => async (operation, ctx) => {
-        if (!isQuery(operation)) return next(operation)
+    const { pool, ...config } = operation.config
 
-        const client = await getClient(ctx)
+    const client = await getClient(ctx, pool)
 
-        return client.query(operation.sql)
-    }
+    return client.query(config)
 }
