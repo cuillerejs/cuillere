d7bdaf1d2d9bffef14f149997c742893d775cf6a
Valentin Cocaud <v.cocaud@gmail.com>
2019-07-13 16:51:18 +0200

    :sprakles: add some mutation tests

--- graphql-example/resolvers.js
+++ graphql-example/resolvers.js
@@ -6,7 +6,7 @@ const makeResolver = makeResolverFactory(makeRunner(queryMiddleware()))
 
 export const resolvers = {
   Query: {
-    hello: makeResolver(function*(_, { name }) {
+    hello: makeResolver(function* (_, { name }) {
       let res = yield query({ text: 'SELECT NOW()', pool: 'foo' })
       res = yield query({ text: 'SELECT NOW()', pool: 'bar' })
       res = yield query({ text: 'SELECT NOW()', pool: 'bar' })
@@ -15,5 +15,26 @@ export const resolvers = {
       } = res
       return `Hello ${name} (${now})`
     }),
+  },
+
+  Mutation: {
+    setupDatabase: makeResolver(function* () {
+      yield query({ pool: 'foo', text: 'CREATE TABLE names (name varchar(250))' })
+    }),
+    addName: makeResolver(function* (_, { name }) {
+      yield query({ pool: 'foo', text: 'INSERT INTO names VALUES ($1)', values: [name] })
+      return name
+    }),
+    setName: makeResolver(function* (_, { before, after }) {
+      const { rows: [updated], } = yield query({
+        pool: 'foo',
+        text: 'UPDATE names SET name = $2 WHERE name = $1 RETURNING name',
+        values: [before, after]
+      })
+      return updated && updated.name
+    }),
+    wait: async () => {
+      await new Promise((resolve) => setTimeout(resolve, 5000))
+    }
   },
 }
--- graphql-example/schema.js
+++ graphql-example/schema.js
@@ -4,4 +4,11 @@ export const typeDefs = gql`
   type Query {
     hello(name: String): String!
   }
+
+  type Mutation {
+    setupDatabase: Boolean
+    addName(name: String!): String!
+    setName(before: String!, after: String!): String
+    wait: String
+  }
 `
