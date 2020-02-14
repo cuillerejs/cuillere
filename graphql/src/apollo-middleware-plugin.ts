c8b900bb57da2812b233570fc44ae5e0d5e271c0
Valentin Cocaud <v.cocaud@gmail.com>
2020-02-14 17:02:16 +0100

    :recycle: works with a collection of middlewares

--- graphql/src/apollo-middleware-plugin.ts
+++ graphql/src/apollo-middleware-plugin.ts
@@ -1,17 +1,21 @@
 import { ApolloServerPlugin } from 'apollo-server-plugin-base'
 
-export const ApolloMiddlewarePlugin = (m1: Middleware, m2: Middleware): ApolloServerPlugin => ({
+export const ApolloMiddlewarePlugin = (...middlewares: Middleware[]): ApolloServerPlugin => {
+  const middlewareChain: Middleware = middlewares.reduceRight(
+    (accF, m) => (ctx, f) => m(ctx, () => accF(ctx, f)),
+  )
+
+  return ({
   requestDidStart: () => ({
     executionDidStart({ context }) {
       const [executionPromise, resolve, reject] = resolvablePromise<void>()
 
-      // FIXME: make it work with a list of middleware
-      m1(context, () => m2(context, () => executionPromise))
+      middlewareChain(context, () => executionPromise)
 
       return err => err ? reject(err) : resolve()
     }
   })
-})
+})}
 
 export interface Middleware {
   <R>(ctx: Record<string, any>, cb: () => Promise<R>): Promise<R>
