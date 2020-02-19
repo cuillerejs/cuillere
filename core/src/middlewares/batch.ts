4bbed4fc47bad81c8df209c9b2e451718dc344fe
Valentin Cocaud <v.cocaud@gmail.com>
2020-02-19 17:56:39 +0100

    :recycle: do not debounce the execution fork

--- core/src/middlewares/batch.ts
+++ core/src/middlewares/batch.ts
@@ -1,30 +1,34 @@
 import { Middleware } from "./middleware"
-import {call, cancel, fork, Run, Call, isCall} from '../cuillere'
+import { GeneratorFunction } from "../generator"
+import {call, execute, fork, Run, Call, isCall} from '../cuillere'
 
-export const batchMiddelware = ({ timeout = 10 } = {}): Middleware =>
+interface BatchOptions {
+  timeout?: number
+}
+
+export const batchMiddelware = ({ timeout }: BatchOptions = {}): Middleware =>
   function* batchMiddelware(operation, ctx: Context, next) {
     if(!ctx[BATCH_CTX]) ctx[BATCH_CTX] = new Map<any, BatchEntry>()
 
     if(isBatchedCall(operation)) {
-      const newFork = yield fork(call(delayBatchExecution, timeout, operation.func))
-
       let entry: any
       if(ctx[BATCH_CTX].has(operation.func)) {
         entry = ctx[BATCH_CTX].get(operation.func)
-        cancel(entry.fork)
       } else {
-        entry = { resolves: [], args: [], fork: newFork }
+        console.log('create fork')
+        entry = { resolves: [], args: [], }
         ctx[BATCH_CTX].set(operation.func, entry)
+        entry.fork = yield fork(call(delayBatchExecution, operation.func, timeout))
       }
 
-      entry.fork = newFork
       entry.args.push(operation.args)
       return new Promise(resolve => entry.resolves.push(resolve))
     }
 
     if(isExecuteBatch(operation)) {
       const entry = ctx[BATCH_CTX].get(operation.func)
-      const result = yield call(operation.func, ...entry.args)
+      ctx[BATCH_CTX].delete(operation.func)
+      const result = yield execute(operation.func(...entry.args))
       entry.resolves.forEach((resolve, i) => resolve(result[i]))
       return
     }
@@ -40,12 +44,11 @@ interface Context {
 }
 
 const isBatchedCall = (operation: any): operation is Call =>
-  isCall(operation) && operation[IS_BATCHED]
+  isCall(operation) && operation.func[IS_BATCHED]
 
-export const batchedCall = (func: any, ...args: any[]): Call => {
-  const c = call(func, ...args)
-  c[IS_BATCHED] = true
-  return c
+export const batched = <T extends GeneratorFunction>(func: T): T => {
+  func[IS_BATCHED] = true
+  return func
 }
 
 const EXECUTE_BATCH = Symbol('EXECUTE_BATCH')
@@ -69,7 +72,7 @@ interface BatchEntry {
   args: any[][]
 }
 
-async function* delayBatchExecution(delay: number, fn: any) {
-  await new Promise(resolve => setTimeout(resolve, delay))
-  yield executeBatch(fn)
+async function* delayBatchExecution(func: GeneratorFunction, delay?: number) {
+  await new Promise(resolve =>  delay ? setTimeout(resolve, delay) : setImmediate(resolve))
+  yield executeBatch(func)
 }
--- core/tests/batch.spec.ts
+++ core/tests/batch.spec.ts
@@ -1,56 +1,87 @@
 /* eslint-env jest */
-import cuillere, { Cuillere } from '../src'
-import { batchedCall, batchMiddelware } from '../src/middlewares/batch'
+import cuillere, { Cuillere, call } from '../src'
+import { batched, batchMiddelware } from '../src/middlewares/batch'
+
+const delay = (timeout: number) => new Promise(resolve => setTimeout(resolve, timeout))
 
 describe('middlewares', () => {
   describe('batch', () => {
     let cllr: Cuillere
     const mock = jest.fn()
 
-    function* fn(...args: any[]) {
+    const fn = batched(function* fn(...args: any[]) {
       mock(...args)
       return [].concat(...args)
-    }
+    })
 
     beforeEach(() => {
-      cllr = cuillere(batchMiddelware())
+      cllr = cuillere(batchMiddelware({ timeout: 0 }))
       mock.mockClear()
     })
 
     it('should call a given operation', async () => {
-      await cllr.start(batchedCall(fn))
+      await cllr.start(call(fn))
 
       expect(mock).toBeCalled()
     })
 
     it('should call only once for multiple batched calls', async () => {
       await Promise.all([
-        cllr.start(batchedCall(fn)),
-        cllr.start(batchedCall(fn)),
-        cllr.start(batchedCall(fn))
+        cllr.start(call(fn)),
+        cllr.start(call(fn)),
+        cllr.start(call(fn))
       ])
 
       expect(mock).toBeCalledTimes(1)
     })
 
-    it('should call with an array of calls', async () => {
+    it('should call with an array of calls with the right length', async () => {
       await Promise.all([
-        cllr.start(batchedCall(fn, 1)),
-        cllr.start(batchedCall(fn, 2)),
-        cllr.start(batchedCall(fn, 3))
+        cllr.start(call(fn, 1)),
+        cllr.start(call(fn, 2)),
+        cllr.start(call(fn, 3))
       ])
 
-      expect(mock).toBeCalledWith([1], [2], [3])
+      expect(mock.mock.calls[0].length).toBe(3)
+    })
+
+    it('should call with all given arguments', async () => {
+      await Promise.all([
+        cllr.start(call(fn, 1)),
+        cllr.start(call(fn, 2)),
+      ])
+
+      expect(mock.mock.calls[0]).toContainEqual([1])
+      expect(mock.mock.calls[0]).toContainEqual([2])
     })
 
     it('should return the right result for each btached call', async () => {
       const result = await Promise.all([
-        cllr.start(batchedCall(fn, 1)),
-        cllr.start(batchedCall(fn, 2)),
-        cllr.start(batchedCall(fn, 3))
+        cllr.start(call(fn, 1)),
+        cllr.start(call(fn, 2)),
+        cllr.start(call(fn, 3))
       ])
 
       expect(result).toEqual([1, 2, 3])
+    })
+
+    it("shouldn't batch calls after timeout", async() => {
+      await cllr.call(fn)
+      await delay(1)
+      await cllr.call(fn)
+      expect(mock).toBeCalledTimes(2)
+    })
+
+    it('should not debounce batch calls', async() => {
+      cllr = cuillere(batchMiddelware({ timeout: 30 }))
+
+      await Promise.all([
+        cllr.call(fn, 1),
+        delay(30).then(() => cllr.call(fn, 2)),
+        delay(45).then(() => cllr.call(fn, 3)),
+      ])
+
+      expect(mock.mock.calls).toEqual([[[1], [2]], [[3]]])
     })
   })
 })
