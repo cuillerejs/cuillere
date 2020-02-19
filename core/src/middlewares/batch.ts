ff4b28b006fd5c60c5dc01f8a6fc5d032ef71249
Valentin Cocaud <v.cocaud@gmail.com>
2020-02-19 15:59:29 +0100

    :truck: reorganize code

--- core/src/middlewares/batch.ts
+++ core/src/middlewares/batch.ts
@@ -1,6 +1,38 @@
 import { Middleware } from "./middleware"
 import {getLocation, call, cancel, fork, Run} from '../cuillere'
 
+
+export const batchMiddelware = ({ timeout = 10 } = {}): Middleware =>
+  function* batchMiddelware(operation, ctx: Context, next) {
+    if(!ctx[BATCH_CTX]) ctx[BATCH_CTX] = new Map<any, BatchEntry>()
+
+    if(isBatchedCall(operation)) {
+      const newFork = yield fork(call(delayBatchExecution, timeout, operation.func))
+
+      let entry: any
+      if(ctx[BATCH_CTX].has(operation.func)) {
+        entry = ctx[BATCH_CTX].get(operation.func)
+        cancel(entry.fork)
+      } else {
+        entry = { resolves: [], args: [], fork: newFork }
+        ctx[BATCH_CTX].set(operation.func, entry)
+      }
+
+      entry.fork = newFork
+      entry.args.push(operation.args)
+      return new Promise(resolve => entry.resolves.push(resolve))
+    }
+
+    if(isExecuteBatch(operation)) {
+      const entry = ctx[BATCH_CTX].get(operation.func)
+      const result = yield call(operation.func, ...entry.args)
+      entry.resolves.forEach((resolve, i) => resolve(result[i]))
+      return
+    }
+
+    return yield next(operation)
+  }
+
 const BATCHED_CALL = Symbol('BATCHED_CALL')
 const BATCH_CTX = Symbol('BATCH_CTX')
 
@@ -50,34 +82,3 @@ async function* delayBatchExecution(delay: number, fn: any) {
   await new Promise(resolve => setTimeout(resolve, delay))
   yield executeBatch(fn)
 }
-
-export const batchMiddelware = ({ timeout = 10 } = {}): Middleware =>
-  function* batchMiddelware(operation, ctx: Context, next) {
-    if(!ctx[BATCH_CTX]) ctx[BATCH_CTX] = new Map<any, BatchEntry>()
-
-    if(isBatchedCall(operation)) {
-      const newFork = yield fork(call(delayBatchExecution, timeout, operation.func))
-
-      let entry: any
-      if(ctx[BATCH_CTX].has(operation.func)) {
-        entry = ctx[BATCH_CTX].get(operation.func)
-        cancel(entry.fork)
-      } else {
-        entry = { resolves: [], args: [], fork: newFork }
-        ctx[BATCH_CTX].set(operation.func, entry)
-      }
-
-      entry.fork = newFork
-      entry.args.push(operation.args)
-      return new Promise(resolve => entry.resolves.push(resolve))
-    }
-
-    if(isExecuteBatch(operation)) {
-      const entry = ctx[BATCH_CTX].get(operation.func)
-      const result = yield call(operation.func, ...entry.args)
-      entry.resolves.forEach((resolve, i) => resolve(result[i]))
-      return
-    }
-
-    return yield next(operation)
-  }
