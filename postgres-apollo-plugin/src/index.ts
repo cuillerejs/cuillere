3c61c8b0cdc033856980682b7693fc7493616448
Valentin Cocaud <v.cocaud@gmail.com>
2020-04-01 11:17:21 +0200

    :sparkles: expose clients from postgres-apollo-plugin

--- postgres-apollo-plugin/src/index.ts
+++ postgres-apollo-plugin/src/index.ts
@@ -1,5 +1,7 @@
+import { GeneratorFunction, get, call } from '@cuillere/core'
 import type { ApolloServerPlugin } from 'apollo-server-plugin-base'
 import { ProviderMiddlewareOptions, TransactionManagerOptions, PoolProvider, TransactionManager, setQueryHandler } from '@cuillere/postgres'
+import { Client } from 'pg'
 
 export const CuillerePostgresApolloPlugin = (
   options: ProviderMiddlewareOptions,
@@ -12,9 +14,17 @@ export const CuillerePostgresApolloPlugin = (
       executionDidStart({ context }) {
         const manager = new TransactionManager(provider, transactionOptions)
         setQueryHandler(context, query => manager.query(query))
+        context.getClient = (pool: string) => manager.getClient(pool)
 
         return err => (err ? manager.rollback() : manager.commit())
       },
     }),
   })
 }
+
+const getClientImpl: GeneratorFunction<[string], Client> = function* getClient(name: string) {
+  const getClientFn = yield get('getClient')
+  return getClientFn(name)
+}
+
+export const getClient = (name: string) => call(getClientImpl, name)
