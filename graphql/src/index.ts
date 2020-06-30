d230a5a641582cfb436b0e6d8a2adfff835b73fb
Nicolas Lepage <19571875+nlepage@users.noreply.github.com>
2020-07-01 00:57:02 +0200

    :bug: GraphQL: isGenrator isn't nullsafe anymore

--- graphql/src/index.ts
+++ graphql/src/index.ts
@@ -1,13 +1,13 @@
 import { Cuillere, isGenerator } from '@cuillere/core'
 
 interface Resolver {
-  (obj: object, args: object, ctx: object, info: object): any
+  (obj: any, args: any, ctx: any, info: any): any
 }
 
 export const makeResolverFactory = (cllr: Cuillere) => (fn: Resolver): Resolver =>
   (obj, args, ctx: CuillereContext, info) => {
     const res = fn(obj, args, ctx, info)
-    if (!isGenerator(res)) return res
+    if (!res || !isGenerator(res)) return res
     // Copy function name on generator for stacktrace
     res['name'] = fn.name // eslint-disable-line dot-notation
     return (ctx.cuillere ?? cllr.ctx(ctx)).start(res as any)
@@ -16,7 +16,7 @@ export const makeResolverFactory = (cllr: Cuillere) => (fn: Resolver): Resolver 
 export const makeResolversTreeFactory = (cllr: Cuillere) => {
   const fnToResolver = makeResolverFactory(cllr)
 
-  const treeToResolversTree = (tree: object) => {
+  const treeToResolversTree = (tree: Record<string, any>) => {
     const resolvers = {}
     Object.entries(tree).forEach(([key, value]) => {
       resolvers[key] = typeof value === 'function' ? fnToResolver(value) : treeToResolversTree(value)
