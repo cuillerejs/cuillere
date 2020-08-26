6f724fb9d06788925053daaabe6072ae8807a4f5
Nicolas Lepage <19571875+nlepage@users.noreply.github.com>
2020-08-26 22:01:38 +0200

    :recycle: Move next to coreHandlers...

--- core/src/operations/next.spec.ts
+++ core/src/operations/next.spec.ts
@@ -1,12 +1,6 @@
-import cuillere, { next, Cuillere } from '..'
+import cuillere, { next } from '..'
 
 describe('next', () => {
-  let cllr: Cuillere
-
-  beforeEach(() => {
-    cllr = cuillere()
-  })
-
   it('should not be allowed outside handlers', async () => {
     let catched: Error
 
@@ -18,7 +12,25 @@ describe('next', () => {
       }
     }
 
-    await cllr.call(test)
+    await cuillere().call(test)
     expect(catched).toStrictEqual(new TypeError('next: should be used only in handlers'))
+  })
+
+  it('should not allow to change operation kind', async () => {
+    let catched: Error
+
+    await cuillere(
+      { handlers: {
+        * '@cuillere/test/test'() {
+          try {
+            yield next({ kind: '@cuillere/test/test2' })
+          } catch (e) {
+            catched = e
+          }
+        },
+      } },
+    ).start({ kind: '@cuillere/test/test' })
+
+    expect(catched).toStrictEqual(new TypeError('next: operation kind mismatch, expected "@cuillere/test/test", received "@cuillere/test/test2"'))
   })
 })
