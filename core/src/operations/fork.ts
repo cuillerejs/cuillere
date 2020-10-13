25aeaa6211a3d4d1d5274f98f5a05fc2293a89c4
Nicolas Lepage <19571875+nlepage@users.noreply.github.com>
2020-10-13 12:37:28 +0200

    :bug: fixes after upgrade...

--- core/src/operations/fork.ts
+++ core/src/operations/fork.ts
@@ -5,7 +5,7 @@ import { Operation, Wrapper, isOfKind, isOperation, coreNamespace } from './oper
 const kind = `${coreNamespace}/fork`
 
 export function fork<Args extends any[], R>(func: GeneratorFunction<Args, R>, ...args: Args): Wrapper<Operation>
-export function fork<Args extends any[], R>(operation: Operation): Wrapper<Operation>
+export function fork(operation: Operation): Wrapper<Operation>
 
 export function fork<Args extends any[], R>(arg0: Operation | GeneratorFunction<Args, R>, ...args: Args) {
   return { kind, operation: isOperation(arg0) ? arg0 : call(arg0, ...args) }
