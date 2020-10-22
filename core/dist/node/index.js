b6dcfe1fa907fd60a41f0ebb7abd8ca6887ad2fd
Nicolas Lepage <19571875+nlepage@users.noreply.github.com>
2020-10-22 09:32:20 +0200

    :alembic: New tsconfig...

--- core/dist/node/index.js
+++ core/dist/node/index.js
@@ -1,2 +1,4 @@
-// eslint-disable-next-line import/no-dynamic-require
-module.exports = require(`./${Number(process.versions.node.split('.')[0]) >= 14 ? '14' : '12'}`)
+/* eslint-disable global-require */
+const nodeMajorVersion = Number(process.versions.node.split('.')[0])
+if (nodeMajorVersion >= 14) module.exports = require('./14')
+else module.exports = require('./12')
