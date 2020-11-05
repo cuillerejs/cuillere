d7dd0bf732c184d10031ade1cdc7f6b15213dc3d
Nicolas Lepage <19571875+nlepage@users.noreply.github.com>
2020-11-05 16:19:41 +0100

    :bug: fix Wrapping accidentally resolvers returning PubSubAsyncIterator (#45)
    * :bug: fix Wrapping accidentally resolvers returning PubSubAsyncIterator
    
    * ✅
    
    * ✅

--- core/src/generator.spec.ts
+++ core/src/generator.spec.ts
@@ -1,16 +1,33 @@
-import { isGenerator } from './generator'
+import { isGenerator, isGeneratorFunction } from './generator'
 
 describe('generator', () => {
-  describe('isGenerator', () => {
-    it('should return true for a generator', () => {
-      function* test() {
-        return true
-      }
+  function* gen() { /* test */ }
+  async function* asyncGen() { /* test */ }
+  function func() { /* test */ }
+  async function asyncFunc() { /* test */ }
+  function makeGenerator(): Generator {
+    return {
+      next() { return undefined },
+      return() { return undefined },
+      throw() { return undefined },
+      [Symbol.iterator]() { return this },
+    }
+  }
 
-      expect(isGenerator(test())).toBe(true)
+  describe('isGenerator', () => {
+    it('should return true for a generator or async generator', () => {
+      expect(isGenerator(gen())).toBe(true)
+      expect(isGenerator(asyncGen())).toBe(true)
     })
 
     it('should return false for non generator values', () => {
+      expect(isGenerator(gen)).toBe(false)
+      expect(isGenerator(asyncGen)).toBe(false)
+      expect(isGenerator(func)).toBe(false)
+      expect(isGenerator(asyncFunc)).toBe(false)
+      expect(isGenerator(makeGenerator)).toBe(false)
+      expect(isGenerator(makeGenerator())).toBe(false)
+      expect(isGenerator([][Symbol.iterator]())).toBe(false)
       expect(isGenerator(Promise.resolve())).toBe(false)
       expect(isGenerator(1)).toBe(false)
       expect(isGenerator('test')).toBe(false)
@@ -20,6 +37,32 @@ describe('generator', () => {
       expect(isGenerator(false)).toBe(false)
       expect(isGenerator(undefined)).toBe(false)
       expect(isGenerator(null)).toBe(false)
+    })
+  })
+
+  describe('isGeneratorFunction', () => {
+    it('should return true for a generator function or async generator function', () => {
+      expect(isGeneratorFunction(gen)).toBe(true)
+      expect(isGeneratorFunction(asyncGen)).toBe(true)
+    })
+
+    it('should return false for non generator values', () => {
+      expect(isGeneratorFunction(gen())).toBe(false)
+      expect(isGeneratorFunction(asyncGen())).toBe(false)
+      expect(isGeneratorFunction(func)).toBe(false)
+      expect(isGeneratorFunction(asyncFunc)).toBe(false)
+      expect(isGeneratorFunction(makeGenerator)).toBe(false)
+      expect(isGeneratorFunction(makeGenerator())).toBe(false)
+      expect(isGeneratorFunction([][Symbol.iterator]())).toBe(false)
+      expect(isGeneratorFunction(Promise.resolve())).toBe(false)
+      expect(isGeneratorFunction(1)).toBe(false)
+      expect(isGeneratorFunction('test')).toBe(false)
+      expect(isGeneratorFunction([])).toBe(false)
+      expect(isGeneratorFunction({})).toBe(false)
+      expect(isGeneratorFunction(true)).toBe(false)
+      expect(isGeneratorFunction(false)).toBe(false)
+      expect(isGeneratorFunction(undefined)).toBe(false)
+      expect(isGeneratorFunction(null)).toBe(false)
     })
   })
 })
