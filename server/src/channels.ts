b8bc09173a08ff1bc5411f98a3e63ccab0108286
Nicolas Lepage <19571875+nlepage@users.noreply.github.com>
2021-05-17 14:46:53 +0200

    :construction:

--- server/src/channels.ts
+++ server/src/channels.ts
@@ -1,11 +1,12 @@
-import { chan, range } from '@cuillere/channels'
+import { chan, range, select } from '@cuillere/channels'
 import { SyntaxError, gql } from 'apollo-server-core'
 import { GraphQLField, GraphQLInterfaceType, GraphQLObjectType } from 'graphql'
 import { SchemaDirectiveVisitor } from 'graphql-tools'
 
-import { assertCuillereSchema } from './schema'
+import { CUILLERE_INSTANCE, isCuillereSchema } from './schema'
 
 export const CUILLERE_CHANNELS = Symbol('CUILLERE_CHANNELS')
+const CUILLERE_CHANNELS_SUBSCRIBERS = Symbol('CUILLERE_CHANNELS_SUBSCRIBERS')
 
 export class ChannelDirective extends SchemaDirectiveVisitor {
   static typeDefs = gql`
@@ -13,22 +14,51 @@ export class ChannelDirective extends SchemaDirectiveVisitor {
   `
 
   visitFieldDefinition(field: GraphQLField<any, any>, details: { objectType: GraphQLObjectType | GraphQLInterfaceType}) {
-    // FIXME uncomment this
-    // assertCuillereSchema(this.schema)
+    const { schema } = this
+
+    if (!isCuillereSchema(schema)) return
 
     if (details.objectType.name !== 'Subscription') {
       throw new SyntaxError(`@channel must be used on Subscription fields only, found on ${details.objectType.name}.${field.name}`)
     }
 
-    if (!(CUILLERE_CHANNELS in this.schema)) this.schema[CUILLERE_CHANNELS] = {}
+    // FIXME use an apollo server plugin (a schema might be reused by more than one server):
+    // - create the chan references when the server starts
+    // - start the infinite loop when server starts
+    // - stop the infinite loop when server stops
+    if (!(CUILLERE_CHANNELS in schema)) {
+      schema[CUILLERE_CHANNELS] = {}
+      schema[CUILLERE_CHANNELS_SUBSCRIBERS] = {}
+
+      schema[CUILLERE_INSTANCE].call(function* () {
+        while (true) { // eslint-disable-line no-constant-condition
+          try {
+            yield select()
+          } catch (e) {
+            // FIXME
+          }
+        }
+      })
+    }
 
     const { bufferCapacity } = this.args
 
-    // FIXME use an apollo server plugin ?
-    // actually a schema might be reused by more than one server
-    // so it might be a good idea to create the chan references in the serverWillStart of a plugin...
-    this.schema[CUILLERE_CHANNELS][field.name] = chan(bufferCapacity)
+    if (!(field.name in schema[CUILLERE_CHANNELS])) {
+      schema[CUILLERE_CHANNELS][field.name] = chan(bufferCapacity)
+      schema[CUILLERE_CHANNELS_SUBSCRIBERS][field.name] = new Set()
 
-    field.subscribe = () => range(this.schema[CUILLERE_CHANNELS][field.name])
+      // FIXME start a runner...
+    }
+
+    field.subscribe = (): AsyncIterator<any> => ({
+      async next() {
+        return it.next()
+      },
+
+      async return() {
+
+      },
+    })
+    // field.subscribe = () => range(this.schema[CUILLERE_CHANNELS][field.name])
   }
 }
