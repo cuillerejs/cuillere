75822320b2795329ec264ff98dd50b2e8a4a3f2f
Valentin Cocaud <v.cocaud@gmail.com>
2021-06-22 23:49:25 +0200

    :green_heart: fix types

--- postgres/src/plugins/crud.ts
+++ postgres/src/plugins/crud.ts
@@ -1,17 +1,19 @@
-import { Plugin, next } from '@cuillere/core'
-import type { Describe } from '@cuillere/crud'
+import { Plugin, next, OperationObject } from '@cuillere/core'
+import type { Crud, Table } from '@cuillere/crud'
 
 import { getPoolsGetter } from '../pools-getter'
 import { query } from './client'
 
+const namespace = '@cuillere/postgres/crud'
+
 export function crudPlugin(): Plugin {
   return {
     handlers: {
-      * '@cuillere/crud/describe'(describe: Describe, ctx) {
+      * [`${namespace}/build`](operation: BuildCrud, ctx) {
         const getPools = getPoolsGetter(ctx)
         if (!getPools) throw new Error('No pools getter in context, you probably forgot to setup a client manager')
 
-        const description = { postgres: {} }
+        const description: Crud = { postgres: {} }
 
         for (const pool of getPools()) {
           const { rows: tables } = yield query({
@@ -24,14 +26,26 @@ export function crudPlugin(): Plugin {
             pool,
           })
 
+          description.postgres[pool] = {}
           for (const { table, schema } of tables) {
-            if (!description.postgres[schema])description.postgres[schema] = {}
-            description.postgres[schema][table] = {}
+            if (!description.postgres[pool][schema]) description.postgres[pool][schema] = {}
+            description.postgres[pool][schema][table] = makeCrud(schema, table)
           }
         }
 
-        return { ...description, ...yield next(describe) }
+        return { ...description, ...yield next(operation) }
       },
     },
   }
+}
+
+export type BuildCrud = OperationObject
+
+export function buildCrud(): BuildCrud {
+  return { kind: `${namespace}/build` }
+}
+
+function makeCrud(schema: string, table: string): Table {
+  // TODO
+  return {}
 }
