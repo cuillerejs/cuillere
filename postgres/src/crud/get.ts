ca4f20d694f6467b092481cad60bf7f0a9338c85
Nicolas Lepage <19571875+nlepage@users.noreply.github.com>
2021-07-19 16:25:10 +0200

    :sparkles: multiple get...

--- postgres/src/crud/get.ts
+++ postgres/src/crud/get.ts
@@ -2,23 +2,24 @@ import { query } from '../plugin'
 import { TableInfo } from './types'
 
 export function makeGet({ pool, schema, table, primaryKey }: TableInfo) {
-  if (primaryKey.length === 0) {
-    return () => {
-      throw new TypeError('No primary key defined')
-    }
-  }
+  if (primaryKey.length === 0) return () => { throw new TypeError('No primary key defined') }
 
   return function* get(id: any) {
     const { rows } = yield query({
       text: `
         SELECT *
         FROM "${schema}"."${table}"
-        WHERE ${primaryKey.map((column, i) => `"${column}" = $${i + 1}`).join(' AND ')}
+        WHERE (${primaryKey.map(column => `"${column}"`)}) = ANY($1)
       `,
-      values: primaryKey.length === 1 ? [id] : primaryKey.map(column => id[column]),
+      values: [[].concat(id).map(idToTuple)],
       pool,
     })
 
     return rows[0]
+  }
+
+  function idToTuple(id: any) {
+    if (primaryKey.length === 1) return [id]
+    return primaryKey.map(column => id[column])
   }
 }
