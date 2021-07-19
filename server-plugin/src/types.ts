20724a9452f7e4d4f8368340164ce3af08586331
Nicolas Lepage <19571875+nlepage@users.noreply.github.com>
2021-07-19 14:37:25 +0200

    🐛 Make examples work

--- server-plugin/src/types.ts
+++ server-plugin/src/types.ts
@@ -1,10 +1,3 @@
-import { ServerPlugin } from './server-plugin'
-
-export interface CuillereConfig {
-  contextKey?: string
-  plugins?: ((srvCtx: ServerContext) => ServerPlugin)[]
-}
-
 export type ServerContext = Map<any, any>
 
 export type OneOrMany<T> = T | T[]
