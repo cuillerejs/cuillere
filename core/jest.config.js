bbc8a625b7654e99310eafee923e2ff84ee0beaa
Valentin Cocaud <v.cocaud@gmail.com>
2021-09-30 21:49:45 +0200

    :green_heart: improve CI with more robust and stable tests and configs (#71)

--- core/jest.config.js
+++ core/jest.config.js
@@ -1,11 +1,17 @@
 const nodeMajorVersion = Number(process.versions.node.split('.')[0])
 
+let tsconfig
+
+if (nodeMajorVersion >= 16) tsconfig = 'tsconfig-node16.json'
+else if (nodeMajorVersion >= 14) tsconfig = 'tsconfig-node14.json'
+else tsconfig = 'tsconfig-node12.json'
+
 module.exports = {
   preset: 'ts-jest',
   testEnvironment: 'node',
   globals: {
     'ts-jest': {
-      tsconfig: `tsconfig-node${nodeMajorVersion}.json`,
+      tsconfig,
     },
   },
 }
