99c4adea43df6054d01b312dee155754f1f73887
Nicolas Lepage <19571875+nlepage@users.noreply.github.com>
2022-01-05 22:50:27 +0100

    :bulb: Documentation for call

--- packages/core/src/operation/call.ts
+++ packages/core/src/operation/call.ts
@@ -10,6 +10,13 @@ export interface CallOperation extends Operation {
   args?: any[]
 }
 
-export function call<Args extends any[], R>(func: GeneratorFunction<Args, R>, ...args: Args): CallOperation {
+/**
+ * Creates a call operation for the given generator function and arguments.
+ * @param func Generator function to be called
+ * @param args Arguments for the generator function
+ * @typeParam Args Arguments type
+ * @returns A new call operation
+ */
+export function call<Args extends any[]>(func: GeneratorFunction<Args>, ...args: Args): CallOperation {
   return { kind: `${CORE_NAMESPACE}/call`, func, args }
 }
