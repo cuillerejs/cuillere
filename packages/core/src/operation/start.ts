2276d52aef4f447b20d18d06baa83d1efd1f4382
Nicolas Lepage <19571875+nlepage@users.noreply.github.com>
2022-01-05 22:22:34 +0100

    :recycle: Internalize start

--- packages/core/src/operation/start.ts
+++ packages/core/src/operation/start.ts
@@ -2,6 +2,9 @@ import { CORE_NAMESPACE } from '../core-namespace'
 import { type Effect } from '../effect'
 import { type WrapperOperation } from './wrapper'
 
+/**
+ * @internal
+ */
 export function start<T extends Effect>(effect: T): WrapperOperation<T> {
   return { kind: `${CORE_NAMESPACE}/start`, effect }
 }
