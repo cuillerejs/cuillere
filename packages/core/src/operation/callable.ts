b54d570b01a48480e06986cbb134527d84adf0b1
Nicolas Lepage <19571875+nlepage@users.noreply.github.com>
2022-01-17 18:10:45 +0100

    :bulb: Document callable

--- packages/core/src/operation/callable.ts
+++ packages/core/src/operation/callable.ts
@@ -7,6 +7,14 @@ function* callableGenerator() {
   yield terminal(yield generator())
 }
 
+/**
+ * Wraps an operation factory to make it [[call | callable]].
+ *
+ * @param factory Operation factory to be wrapped.
+ * @typeParam Args Factory's arguments type.
+ * @typeParam T Operation type returned by the factory.
+ * @returns A new function with the same signature as `factory`.
+ */
 export function callable<Args extends any[] = any[], T extends Operation = Operation>(
   factory: (...args: Args) => T,
 ): (...args: Args) => T & Generator<void, Operation> {
