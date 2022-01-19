d2b5f2c3ce720bfbba97cac23656abe16d116ed0
Nicolas Lepage <19571875+nlepage@users.noreply.github.com>
2022-01-19 09:18:45 +0100

    :bulb: Document Operation and isOperation

--- packages/core/src/operation/operation.ts
+++ packages/core/src/operation/operation.ts
@@ -1,10 +1,24 @@
 /**
+ * Base interface for any operation object.
+ *
+ * An operation object may be yielded as an effect to perform some kind of operation.
+ *
  * @category for operations
  */
 export interface Operation {
+
+  /**
+   * Operation's kind.
+   */
   readonly kind: `@${string}`
 }
 
+/**
+ * Checks if `value` is an [[Operation]].
+ *
+ * @param value Value to be checked, must not be `null` or `undefined`.
+ * @returns `true` if `value` has a property named `kind`.
+ */
 export function isOperation(value: any): value is Operation {
   return 'kind' in value
 }
