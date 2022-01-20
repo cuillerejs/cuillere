57b50286edd428835643060d2034fa61f81e0410
Nicolas Lepage <19571875+nlepage@users.noreply.github.com>
2022-01-20 22:11:22 +0100

    :memo: Add a batching example

---
+++ examples/batching/fetch.js
@@ -0,0 +1,28 @@
+import { all, batched, cuillere } from '@cuillere/core'
+import fetch from 'node-fetch'
+
+const getBeers = batched(async function* getBeers(calls) {
+  const ids = calls.flat()
+  console.log(`Fetching ${ids.length} beers...`)
+
+  const res = await fetch(`https://api.punkapi.com/v2/beers?ids=${ids.join('|')}`)
+  const beers = await res.json()
+
+  return calls.map(ids => ids.map(id => beers.find(beer => beer.id === id)))
+})
+
+// Generate beer drinkers with random favourite beers
+const beerDrinkers = Array.from(['Valou', 'Yoann', 'Nico', 'Sev'], name => ({
+  name,
+  favouriteBeers: Array.from({ length: 5 }, () => Math.ceil(Math.random() * 325 + 1)),
+}))
+
+cuillere().call(function* () {
+  // Calling getBeers() once per drinker concurrently should result in one fetch
+  const allBeers = yield all(beerDrinkers.map(({ favouriteBeers }) => getBeers(...favouriteBeers)))
+
+  beerDrinkers.forEach(({ name }, index) => {
+    console.log(`${name}'s favourite beers are:`)
+    allBeers[index].forEach(beer => console.log(`  ${beer.name}, "${beer.tagline}"`))
+  })
+})
