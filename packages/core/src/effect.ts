3d41ffd3fe24cd03282d62465cfe3e79d1d6b0c3
Nicolas Lepage <19571875+nlepage@users.noreply.github.com>
2022-01-20 18:29:36 +0100

    :bulb: Effect and isEffect

--- packages/core/src/effect.ts
+++ packages/core/src/effect.ts
@@ -1,8 +1,19 @@
 import { type Generator, isGenerator } from './generator'
 import { type Operation, isOperation } from './operation'
 
+/**
+ * Effect is a union of all yieldable types.
+ *
+ * @typeParam R Effect's return type.
+ */
 export type Effect<R = any> = Operation | Generator<R>
 
+/**
+ * Checks if `value` is an [[Effect]].
+ *
+ * @param value Value to be checked.
+ * @returns `true` if value is an [[Effect]], false otherwise.
+ */
 export function isEffect(value: any): value is Effect {
   return isOperation(value) || isGenerator(value)
 }
